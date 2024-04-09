eca8fce2595d5c3b63c73c3769ff57fd77d67d5f
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-04-09 09:20:20 -0600

    get build working

--- src/function-calling.ts
+++ src/function-calling.ts
@@ -1,9 +1,57 @@
-import Anthropic from "@anthropic-ai/sdk";
+import Anthropic from '@anthropic-ai/sdk';
+import { Tool } from '@anthropic-ai/sdk/resources/beta/tools/messages.mjs';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const anthropic = new Anthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY || "",
-})
+  apiKey: process.env.ANTHROPIC_API_KEY || '',
+});
+
+const MODEL_NAME = 'claude-3-haiku-20240307';
 
 function getWeather(location: string) {
+  const tools: Tool[] = [
+    {
+      name: 'get_weather',
+      description: 'Get the weather in celsius for a given location',
+      input_schema: {
+        type: 'object',
+        properties: {
+          location: {
+            type: 'string',
+            description: 'The location to get the weather for',
+          },
+          unit: {
+            type: 'string',
+            enum: ['celsius', 'fahrenheit'],
+            description: 'The unit to return the temperature in',
+          },
+        },
+      },
+    },
+  ];
 
+  const query = `
+  <text>
+   ${location}
+  </text>
+
+  Use the get_weather tool.
+  `;
+
+  const response = anthropic.beta.tools.messages.create({
+    model: MODEL_NAME,
+    max_tokens: 4096,
+    tools,
+    messages: [
+      {
+        role: 'user',
+        content: query,
+      },
+    ],
+  });
+
+  return response;
 }
+
+console.log(getWeather('Guadalajara, México'));
